0f12896bfa5cea44657b0d16ba7a8fb89ad9ecee
Amit Mondal <mandalamit325@gmail.com>
2022-12-14 14:08:11 +0000

    some changes

--- front/src/Components/WidgetContent.js
+++ front/src/Components/WidgetContent.js
@@ -6,8 +6,8 @@ function WidgetContent(){
             <div className="widget__content">
                 <img src="https://cdn.pixabay.com/photo/2016/12/13/21/20/alien-1905155__340.png" className="img"/>
                 <div className="widget__contentTitle">
-                    <h5>Mobile App programer</h5>
-                    <p>The best mobile app development company</p>
+                    <h5>Answered Questions</h5>
+                    {/* <p>The best mobile app development company</p> */}
                 </div>
             </div>
 
